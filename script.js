8f3fff700c67249ddbbae3551356ee11e4a75148
Akmaljon0526 <norkuziyevakmal@gmail.com>
2025-08-25 15:12:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let ism = prompt("Ismingiz nima ?")
+let kol = prompt( "Yoshingiz nechida ?")
+let nil = prompt( "Manzilingiz qayerda ?")
+let kop = prompt("Qaysi maktabda o'qiysiz")
+
+
+
+
+
+
+
+
+console.log("hush kelibsiz:" + ism)
+console.log( "17" + kol)
